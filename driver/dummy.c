aac252605cb9b5bf37c70754f0a1fec287ad89fc
task4233 <29667656+task4233@users.noreply.github.com>
2022-03-03 17:21:50 +0900

    feat: add interrupt handler

--- driver/dummy.c
+++ driver/dummy.c
@@ -2,16 +2,27 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "platform.h"
+
 #include "util.h"
 #include "net.h"
 
 #define DUMMY_MTU UINT16_MAX /* maximum size of ID datagram */
+
+#define DUMMY_IRQ INTR_IRQ_BASE
 
 static int dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
 {
     debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
     debugdump(data, len);
     // drop data
+    intr_raise_irq(DUMMY_IRQ);
+    return 0;
+}
+
+static int dummy_isr(unsigned int irq, void *id)
+{
+    debugf("irq=%u, dev=%s", irq, ((struct net_device *)id)->name);
     return 0;
 }
 
@@ -42,6 +53,7 @@ struct net_device *dummy_init(void)
         return NULL;
     }
 
+    intr_request_irq(DUMMY_IRQ, dummy_isr, INTR_IRQ_SHARED, dev->name, dev);
     debugf("initialized, dev=%s", dev->name);
     return dev;
 }
--- net.c
+++ net.c
@@ -140,6 +140,12 @@ int net_run(void)
 {
     struct net_device *dev;
 
+    if (intr_run() == -1)
+    {
+        errorf("intr_run() failure");
+        return -1;
+    }
+
     debugf("open all devices...");
     for (dev = devices; dev; dev = dev->next)
     {
@@ -157,11 +163,17 @@ void net_shutdown(void)
     {
         net_device_close(dev);
     }
+    intr_shutdown();
     debugf("shutting down");
 }
 
 int net_init(void)
 {
+    if (intr_init() == -1)
+    {
+        errorf("intr_init() failure");
+        return -1;
+    }
     infof("initialized");
     return 0;
 }
